e0d4f4ee43445ae927f1cc2c7fe6ef9ab4c2a32a
Riyaj Chaulagain <criyaj2017@gmail.com>
2025-06-03 12:50:24 -0500

    change in package.json

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
 "use client";
 import Landing from "@/components/Home/Landing";
-import Link from 'next/link';
-import Image from 'next/image';
-
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import app from "./Shared/firebaseConfig";
-import { useEffect, useState } from "react"
 
 
 export default function HomePage() {
