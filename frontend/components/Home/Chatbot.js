0ebae6158e7242ae82dabc0cf33b38814bfd82f6
Riyaj Chaulagain <criyaj2017@gmail.com>
2025-06-04 13:15:02 -0500

    change utilis into utils

--- frontend/components/Home/Chatbot.js
+++ frontend/components/Home/Chatbot.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { sendMessageToBot } from "@/app/utils/chatbot";
+import { sendMessageToBot } from '../../app/utils/chatbot';
+
 
 export default function Chatbot() {
   const [messages, setMessages] = useState([]);
